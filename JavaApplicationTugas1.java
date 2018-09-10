d139c0c778f45c4da00a8e220e5f139cd707a556
saddoni <43126550+saddoni@users.noreply.github.com>
2018-09-10 05:23:06 +0000

    Add files via upload

---
+++ JavaApplicationTugas1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplicationtugas1;
+
+/**
+ *
+ * @author win 8.1
+ */
+public class JavaApplicationTugas1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+    int w = 4;
+    for (int i=0; i<w; i++) {
+        for (int j=0; j<=i; j++) {
+        System.out.print("*");
+        }
+        System.out.println();
+        }
+        
+    }
+    
+}
